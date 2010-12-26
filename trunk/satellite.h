d8b3a53c7c7d3bf68150b68d9674141ec113af94
azzuro.huo <azzuro.huo@7e4267a8-ba5b-818a-9e3d-f8ab7eb9eaf8>
2010-12-26 12:11:49 +0000

    add two body dynamics. add satellite configuration file parsing function.

--- trunk/satellite.h
+++ trunk/satellite.h
@@ -1,7 +1,9 @@
 class CSatellite {
   private:
-    double m_fpState[6]; // State vector of the satellite. state = {r_x, r_y, r_z, v_x, v_y, v_z}.
-    double m_dbStep; // Time step size of satellite Euler method.
+    double m_dbMC; // Mass of the central body in kg.
+    double m_dbMS; // Mass of the satellite in kg.
+    double m_fpState[6]; // State vector of the satellite in m or m/s.. state = {r_x, r_y, r_z, v_x, v_y, v_z}.
+    double m_dbStep; // Time step size of satellite Euler method in second.
     unsigned long long m_u64Clock; // Intrinsic clock. time = clock * step.
   public:
     CSatellite();
--- trunk/simulate.cpp
+++ trunk/simulate.cpp
@@ -5,10 +5,10 @@ int main() {
   CSatellite sat;
   double fpState[6];
   unsigned long long i;
-  for(i=1;i<10;i++) {
-    sat.go(10);
+  for(i=1;i<100;i++) {
+    sat.go(1);
     sat.getState(fpState);
-    cout << fpState[0] << ", " << fpState[1] << ", " << fpState[2] << "; " << fpState[3] << ", " << fpState[4] << ", " << fpState[5] << "." << endl;
+    cout << fpState[0] << ", " << fpState[1] << ", " << fpState[2] << ", " << fpState[3] << ", " << fpState[4] << ", " << fpState[5] << "." << endl;
   }
   return 0;
 }
