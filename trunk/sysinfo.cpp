e4d056ed7721bcbe8adfebf07cf0f7230aebbb75
azzuro.huo <azzuro.huo@7e4267a8-ba5b-818a-9e3d-f8ab7eb9eaf8>
2010-12-28 09:09:23 +0000

    minor changes.

--- trunk/sysinfo.cpp
+++ trunk/sysinfo.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 int main(void) {
   cout << "Size of unsigned long long: " << sizeof(unsigned long long) << " byte(s)"<< endl;
   cout << "Size of double: " << sizeof(double) << " byte(s)"<< endl;
