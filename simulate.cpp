8f26bf08487854e8d9cf3493c84097f730239c73
azzuro.huo <azzuro.huo@7e4267a8-ba5b-818a-9e3d-f8ab7eb9eaf8>
2010-12-29 13:29:02 +0000

    minor changes.

--- simulate.cpp
+++ simulate.cpp
@@ -1,8 +1,29 @@
 #include "satellite.h"
+#include <cstdlib>
 #include <iostream>
 using namespace std;
-int main() {
-  CSatellite sat;
-  sat.simulate(86400.0, 1000);
+int main(int argc, char * argv[]) {
+  double dbSimTime;
+  unsigned long u32Inc;
+  bool bCmdIsGood = true;
+  if(argc == 3) {
+    dbSimTime = atof(argv[1]);
+    u32Inc = (unsigned long)(atol(argv[2]));
+    if(dbSimTime == 0.0 || u32Inc == 0) {
+      bCmdIsGood = false;
+    }
+  }else {
+    bCmdIsGood = false;
+  }
+  if(bCmdIsGood) {
+    CSatellite sat;
+    sat.simulate(dbSimTime, u32Inc);
+  }else {
+    cout << "Usage: \n";
+    cout << "======\n";
+    cout << "simulate T N\n";
+    cout << "  T - End time of simulation.\n";
+    cout << "  N - Stepsize is set to integral N times period of pulsars.\n";
+  }
   return 0;
 }
